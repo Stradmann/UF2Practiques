a2db3ac003ef9260a72313903dc0e5522ea798b6
ausias <ausias@h7-32.xiam.cat>
2022-01-14 14:00:34 +0100

    1 acabado

--- UF2Practiques/src/Practica6/Exercici1.java
+++ UF2Practiques/src/Practica6/Exercici1.java
@@ -10,5 +10,20 @@ package Practica6;
  * @author enric
  */
 public class Exercici1 {
-    
+
+    public static void QuiEsIntegrantDAquestGrup(int numero) {
+        switch (numero) {
+            case 1:
+                System.out.println("Enric");
+                break;
+            case 2:
+                System.out.println("Yenifer");
+                break;
+            case 3:
+                System.out.println("Andrés");
+                break;
+            default:
+                System.out.println("Has introduit un numero incorrecte ja que només hi han 3 integrants en el grup");
+        }
+    }
 }

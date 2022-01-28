824e5853fc4ff06a3310d6244c142eed7169390a
ausias <ausias@h7-31.xiam.cat>
2022-01-28 13:49:51 +0100

    afegit exercici tetris enunciat

---
+++ UF2Practiques/src/Practica10/Exercici1.java
@@ -0,0 +1,44 @@
+/*EL TETRIS
+El 6 de juny de 1984, Alekséi Pázhitnov, un enginyer
+informàtic rus de 29 anys en una avorrida tarda al servei del
+govern rus, va desenvolupar un videojoc molt simple que
+acabaria sent un èxit de vendes. Com que ho va fer amb
+recursos del govern rus, el pobre home no ve rebre ni un
+cèntim i tots els guanys van anar a parar al govern rus. Fart
+d’aquesta situació, l’Alekséi va marxar als Estats Units l’any
+1991. Microsoft el va fitxar l’any 1996 per col·laborar en el
+desenvolupament de la Xbox 360. També al 1996, juntament amb Henk Rogers, van
+aconseguir recuperar els drets d’autor d’aquest videojoc, però ja era massa tard perquè
+corrien diferents versions de codi lliure.
+Es demana que desenvolupeu una versió simple d’aquest joc. El programa demanarà
+el tamany del tauler de joc i anirà mostrant aleatòriament una sèrie de peces que, en
+aquesta versió simplificada, no podran rotar. Aquestes peces les mourem N posicions
+a la dreta o a l’esquerra i les deixarem caure. En el moment que una peça toqui el fons
+o alguna de les peces ja col·locades, la peça aturarà la seva caiguda. Si s’aconsegueix
+que una línia horitzontal quedi completament plena, aquesta línia desapareixerà del
+taulell i les peces (o restes de peces) que hi ha al damunt cauen fins tocar el fons o
+alguna altra peça. El programa finalitza quan alguna peça arriba al límit superior del
+taulell i, per tant, ja no podem col·locar cap més.
+El conjunt de peces del que disposeu és el següent:
+Una possible versió del programa en pseudo-codi podria ser la següent:
+Programa Tetris
+DemanarTamanyTaulell
+Mentre ( no FinalPartida) fer
+MostrarPeça
+MourePeçaCostats
+FerCaureLaPeça
+MostrarTaulell
+ Fi Mentre
+Fi Programa
+Basant-te en aquest pseudo-codi, acaba de desenvolupar aquesta versió simplificada
+del Tetris, aplicant les tècniques de descomposició en subproblemes. Recorda que cap
+funció o procediment hauria de tenir més de 10 línies de codi.*/
+
+package Practica10;
+
+
+public class Exercici1 {
+    public static void main(String[] args){
+        
+    }
+}
